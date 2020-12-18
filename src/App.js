5edd2c1fd6444c34060ddd684bcbe203e3915341
jonathanhumphrey <jlh473@txstate.edu>
2020-12-18 17:12:03 -0600

    buns and the button to enable them

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        buns
+      </div>
+      <button>enable buns</button>
+    </div>
+  );
+}
+
+export default App;
